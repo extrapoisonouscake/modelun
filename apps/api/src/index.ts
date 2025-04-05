9fb9dbc63ec052f7b802aa1e51419a603ac3fe95
Vladislav Gubarev <extrapoisonouscake@gmail.com>
2025-04-05 14:47:36 -0700

    update envs

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -4,7 +4,7 @@ import cors from "cors";
 import "dotenv/config";
 import express from "express";
 import http from "http";
-import { PORT } from "./constants";
+import { isProd, PORT } from "./constants";
 
 import { initializeSocketIO } from "./io";
 import { createContext } from "./routes/context";
@@ -13,13 +13,17 @@ const app = express();
 app.use(cookieParser());
 export const server = http.createServer(app);
 initializeSocketIO(server);
+const CLIENT_DOMAIN = process.env.CLIENT_DOMAIN;
+if (isProd && !CLIENT_DOMAIN) {
+  throw new Error("CLIENT_DOMAIN is not set");
+}
 const handler = trpcExpress.createExpressMiddleware({
   onError(e) {
     console.error(e);
   },
   router: appRouter,
   middleware: cors({
-    origin: "http://localhost:3001",
+    origin: CLIENT_DOMAIN || "http://localhost:3001",
     credentials: true,
   }),
 
--- apps/client/src/utils/trpc.ts
+++ apps/client/src/utils/trpc.ts
@@ -5,7 +5,7 @@ import { createTRPCClient, httpBatchLink } from "@trpc/client";
 export const trpcClient = createTRPCClient<AppRouter>({
   links: [
     httpBatchLink({
-      url: "http://localhost:3002/trpc",
+      url: import.meta.env.VITE_TRPC_URL || "http://localhost:3002/trpc",
       fetch(url, options) {
         return fetch(url, {
           ...options,
