e0ba195bf68fa8e668d85443c6e1994a6cec6191
Vladislav Gubarev <extrapoisonouscake@gmail.com>
2025-04-05 13:50:53 -0700

    change port

--- apps/api/src/constants.ts
+++ apps/api/src/constants.ts
@@ -1,6 +1,6 @@
 import { countries } from "countries-list";
 
-export const PORT = +(process.env.PORT || 3002);
+export const PORT = +(process.env.PORT || 3000);
 export const isDev = process.env.NODE_ENV === "development";
 export const isProd = process.env.NODE_ENV === "production";
 export const CHAIR_IDENTIFIER = "CHAIR";
