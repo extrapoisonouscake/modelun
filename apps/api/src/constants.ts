dfd783ec7923e613aa0ab40f9528ae89f3668b6e
Vladislav Gubarev <extrapoisonouscake@gmail.com>
2025-04-05 21:31:08 -0700

    fix overlapping error

--- apps/api/src/constants.ts
+++ apps/api/src/constants.ts
@@ -7,8 +7,6 @@ export const CHAIR_IDENTIFIER = "CHAIR";
 export const COUNTRY_CODES = Object.keys(countries);
 
 const DOMAIN_NAME = process.env.DOMAIN_NAME;
-if (isProd && !DOMAIN_NAME) {
-  throw new Error("DOMAIN_NAME is not set");
-}
+
 export const CLIENT_ORIGIN =
   `https://${DOMAIN_NAME}` || "http://localhost:3001";
