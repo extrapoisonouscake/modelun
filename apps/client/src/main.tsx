b32de32682e05f305ebb0a1319baaed0b268c07a
Vladislav Gubarev <extrapoisonouscake@gmail.com>
2025-04-05 22:36:02 -0700

    remove unused import

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,17 +1,10 @@
-import { CookiesProvider } from "react-cookie";
 import { createRoot } from "react-dom/client";
 import { App } from "./app";
 import "./index.css";
 const el = document.getElementById("root");
 if (el) {
   const root = createRoot(el);
-  root.render(
-    // <React.StrictMode>
-    <CookiesProvider>
-      <App />
-    </CookiesProvider>
-    // </React.StrictMode>
-  );
+  root.render(<App />);
 } else {
   throw new Error("Could not find root element");
 }
