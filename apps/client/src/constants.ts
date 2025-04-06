7284568efce7dcf92e5076851c2db8b861b2482a
Vladislav Gubarev <extrapoisonouscake@gmail.com>
2025-04-05 17:57:27 -0700

    fix env variables retrieval

--- apps/client/src/constants.ts
+++ apps/client/src/constants.ts
@@ -2,4 +2,4 @@ import { countries } from "countries-list";
 
 export const COUNTRY_CODES = Object.keys(countries);
 export const API_URL =
-  `https://api.${process.env.VITE_DOMAIN_NAME}` || "http://localhost:3000";
+  `https://api.${import.meta.env.VITE_DOMAIN_NAME}` || "http://localhost:3000";
