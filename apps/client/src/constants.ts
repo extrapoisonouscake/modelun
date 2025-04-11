8d58921c4530a8571f62812d44c152eed4ed09cf
Vlad Gubarev <extrapoisonouscake@gmail.com>
2025-04-11 11:04:29 -0700

    fix

--- apps/client/src/constants.ts
+++ apps/client/src/constants.ts
@@ -3,5 +3,5 @@ import { countries } from "countries-list";
 export const COUNTRY_CODES = Object.keys(countries);
 const DOMAIN_NAME = import.meta.env.VITE_DOMAIN_NAME;
 export const API_URL = DOMAIN_NAME
-  ? `https://api.${import.meta.env.VITE_DOMAIN_NAME}`
+  ? `https://${import.meta.env.VITE_DOMAIN_NAME}/api`
   : "http://localhost:3000";
